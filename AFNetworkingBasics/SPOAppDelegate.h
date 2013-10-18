a07b4486890c877b56dd387470cd991fc2339a35
Sendoa Portuondo <sendoa@gmail.com>
2013-10-18 09:30:28 +0200

    Initial Commit

---
+++ AFNetworkingBasics/SPOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPOAppDelegate.h
+//  AFNetworkingBasics
+//
+//  Created by Sendoa Portuondo on 18/10/13.
+//  Copyright (c) 2013 Sendoa Portuondo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFNetworkingBasicsTests/AFNetworkingBasicsTests.m
@@ -0,0 +1,34 @@
+//
+//  AFNetworkingBasicsTests.m
+//  AFNetworkingBasicsTests
+//
+//  Created by Sendoa Portuondo on 18/10/13.
+//  Copyright (c) 2013 Sendoa Portuondo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNetworkingBasicsTests : XCTestCase
+
+@end
+
+@implementation AFNetworkingBasicsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
